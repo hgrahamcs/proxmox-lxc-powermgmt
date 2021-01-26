b06d71c1888d1e330b75d255f03f37cfce764a81
Henry G <8182383+hgrahamcs@users.noreply.github.com>
2021-01-25 18:22:33 -0600

    Add files via upload

--- lxcvpn.sh
+++ lxcvpn.sh
@@ -13,7 +13,7 @@ startVPNLXC(){
 }
 
 stopVPNLXC(){
-	data=`curl -s -k --location --request POST "$HOST/api2/json/nodes/vmh/lxc/110/status/stop" \
+	data=`curl -s -k --location --request POST "$HOST/api2/json/nodes/$NODE/lxc/$VMID/status/stop" \
 		--header "Authorization: PVEAPIToken=$PVE_USER_NAME@$PVE_USER_AUTH_REALM!$PVE_USER_TOKEN=$PVE_USER_TOKEN_SECRET"`
 }
 
